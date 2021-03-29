219d2c97e47f37f64ad3e2448a4bf988978737b1
zhenuua <zhenuua@gmail.com>
2021-03-29 21:33:34 +0300

    feat: do task simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
-  getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
-};
+  chain:[],
+   getLength() {
+    return this.chain.length
+   },
+   addLink(value) {
+     this.chain.push(value)
+     return this
+   },
+   removeLink(position) {
+     if (position <= 0 || typeof position == 'string'){
+       this.chain =[];
+       throw new Error();
+     }
+     this.chain.splice(position-1, 1);
+     return this
+   },
+   reverseChain() {
+     this.chain.reverse()
+     return this
+   },
+   finishChain() {
+
+     let xxx = this.chain
+     .map((i) => `( ${i} )`)
+     .join('~~') 
+     this.chain =[];
+     return xxx;
+   }
+ };
 
 module.exports = chainMaker;
