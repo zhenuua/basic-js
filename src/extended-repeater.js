a483bbc9e12415f1e3bb3d9372ed70a720809d83
zhenuua <zhenuua@gmail.com>
2021-03-29 22:15:02 +0300

    feat: do task extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function repeater(str, options) {
+  if (str === true) {
+    str = 'true';
+  }
+  if (str === null) {
+    str = 'null';
+  }
+  if (!options.separator) {
+    options.separator = '+'
+  }
+  if (!options.additionSeparator) {
+    options.additionSeparator = "|";
+  }
+  if (!options.additionRepeatTimes && options.addition) {
+    options.additionRepeatTimes = 1;
+  }
+
+  for (let j = 0; j < options.additionRepeatTimes; j++) {
+    str += options.addition
+    if (j !== options.additionRepeatTimes - 1) {
+      str += options.additionSeparator
+    }
+  }
+
+  const a = str
+  for (let i = 0; i < options.repeatTimes - 1; i++) {
+    if (i !== options.repeatTimes - 1) {
+      str += options.separator
+    }
+    str += a
+  }
   
+  return str
+}
