eadc2476c3f26adc9613da30f93f81ef65f0a28d
zhenuua <zhenuua@gmail.com>
2021-03-29 21:35:01 +0300

    feat: do part of transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error();
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '--discard-next') {   //исключает следующий за ней элемент исходного массива из преобразованного массива.
+      arr.splice([i + 1], 1);
+      arr.splice([i], 1);
+    }
+    if (arr[i] === '--discard-prev') { //исключает предшествующий ей элемент исходного массива из преобразованного массива
+      arr.splice([i], 1);
+      arr.splice([i - 1], 1);
+      
+    }
+    if (arr[i] === '--double-next') {  //удваивает следующий за ней элемент исходного массива в преобразованном массиве.
+      arr.splice([i], 1);
+      arr.splice([i], 0, arr[i]);
+    }
+    if (arr[i] === '--double-prev') { //удваивает предшествующий ей элемент исходного массива в преобразованном массиве
+      arr.splice([i], 1);
+      arr.splice([i], 0, arr[i - 1]);
+    }
+  } return arr
 };
